d86413b0ffb86bdcd0ced1063b5f1cf33e021b39
novakboh <170075016+novakboh@users.noreply.github.com>
2025-04-18 15:31:49 +0300

    Added CRUD

--- main.js
+++ main.js
@@ -12,9 +12,59 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.pznl908.mongodb.net/?retryW
 const bookSchema = new mongoose.Schema({
     title: String,
     author: String,
-    available: String
+    available: Boolean
 });
 const Book = mongoose.model('Book', bookSchema);
+
+app.post('/books', async (req, res) => {
+    try {
+        const { title, author, available = true } = req.body;
+        const newBook = new Book({ title, author, available });
+        await newBook.save();
+        res.status(201).json({ message: "Book added", book: newBook });
+    } catch (error) {
+        res.status(400).json({ error: "Invalid data format" });
+    }
+}); 
+
+app.get('/books', async (req, res) => {
+    const books = await Book.find();
+    res.status(200).json({ books });
+});
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const { id } = req.body;
+        const book = await Book.findById(id);
+        if (book) res.status(200).json({ book });
+        else res.status(404).json({ error: "Not found" });
+    } catch (error) {
+        res.status(400).json({ error: "Invalid data format" });
+    }
+});
+
+app.put('/books/:id', async (req, res) => {
+    try {
+        const { id, title, author, available } = req.body;
+        await Book.findByIdAndUpdate(id, { title, author, available });
+        const book = await Book.findById(id);
+        if (book) res.status(200).json({ book });
+        else res.status(404).json({ error: "Not found" });
+    } catch (error) {
+        res.status(400).json({ error: "Invalid data format" });
+    }
+});
+
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const { id } = req.body;
+        const book = await Book.findByIdAndDelete(id);
+        if (book) res.status(204).end();
+        else res.status(404).json({ error: "Not found" });
+    } catch (error) {
+        res.status(400).json({ error: "Invalid data format" });
+    }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
